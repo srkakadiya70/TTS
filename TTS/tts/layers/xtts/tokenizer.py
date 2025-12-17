a9a5f9b946c259e20ed799669734042986ca8c2f
sahilpatel2847 <sahilpatel2847@net.com>
2025-12-18 00:04:39 +0530

    changed english language token to 2500

--- TTS/tts/layers/xtts/tokenizer.py
+++ TTS/tts/layers/xtts/tokenizer.py
@@ -32,7 +32,7 @@ def get_spacy_lang(lang):
         return English()
 
 
-def split_sentence(text, lang, text_split_length=250):
+def split_sentence(text, lang, text_split_length=2500):
     """Preprocess the input text"""
     text_splits = []
     if text_split_length is not None and len(text) >= text_split_length:
@@ -595,7 +595,7 @@ class VoiceBpeTokenizer:
         if vocab_file is not None:
             self.tokenizer = Tokenizer.from_file(vocab_file)
         self.char_limits = {
-            "en": 250,
+            "en": 2500,
             "de": 253,
             "fr": 273,
             "es": 239,

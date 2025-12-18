2560152acd6404fbc946dc023142d3fe8781a46c
sahilpatel2847 <sahilpatel2847@net.com>
2025-12-18 23:42:29 +0530

    Add Hindi regex pattern for ordinal numbers in tokenizer
    - Introduced a regular expression for handling ordinal numbers in Hindi.
    - Enhanced the tokenizer's language support for improved text processing.

--- TTS/tts/layers/xtts/tokenizer.py
+++ TTS/tts/layers/xtts/tokenizer.py
@@ -473,6 +473,7 @@ _ordinal_re = {
     "tr": re.compile(r"([0-9]+)(\.|inci|nci|uncu|üncü|\.)"),
     "hu": re.compile(r"([0-9]+)(\.|adik|edik|odik|edik|ödik|ödike|ik)"),
     "ko": re.compile(r"([0-9]+)(번째|번|차|째)"),
+    "hi": re.compile(r"([0-9]+)(वाँ|वीं|वें|वां|वें|वाँ|वा|वे)"),
 }
 _number_re = re.compile(r"[0-9]+")
 _currency_re = {
